e18c9c9d9bce86af824c22768bdd8d78d64c3e18
saikumarsunka455 <saikumarsunka455@gmail.com>
2025-08-28 14:43:53 +0530

    Create eth_mac_sequence.sv

---
+++ eth_mac_sequence.sv
@@ -0,0 +1,17 @@
+//======================ethernet mac sequencer =================
+
+
+class eth_mac_sequencer extends uvm_sequencer #(eth_sequence_item);
+
+//=====================factory registration ============================
+
+	`uvm_component_utils(eth_mac_sequencer)
+
+//============================construction========================
+
+	function new(string name="",uvm_component parent);
+		super.new(name,parent);
+	endfunction
+
+endclass
+
