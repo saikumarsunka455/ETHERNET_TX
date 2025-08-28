b4e4802ef163b9a73dcd78b1d0f145e12128e586
saikumarsunka455 <saikumarsunka455@gmail.com>
2025-08-28 14:51:10 +0530

    Create eth_mem_sequencer.sv

---
+++ eth_mem_sequencer.sv
@@ -0,0 +1,13 @@
+class eth_mem_sequencer extends uvm_sequencer #(eth_sequence_item);
+
+	`uvm_component_utils(eth_mem_sequencer)
+
+	function new (string name="eth_mem_sequencer",uvm_component parent);
+			super.new(name,parent);
+
+	endfunction
+
+
+
+
+endclass
